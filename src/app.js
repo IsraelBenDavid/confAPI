6dc3bc9a76d39cb14f7f923840cd2aad2f24d48c
Israel Ben David <israel1011998@gmail.com>
2022-12-05 20:41:07 +0200

    added user rout to app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 require('./db/mongoose')
 const confessionRouter = require('./routers/confession')
+const userRouter = require('./routers/user')
 
 const app = express()
 
 app.use(express.json())
 app.use(confessionRouter)
+app.use(userRouter)
 
 const port = process.env.PORT
 
